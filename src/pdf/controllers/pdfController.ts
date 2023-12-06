58d608ac09bc5479f4cc4a689a5360b48b4bd08b
Vinicius Koiti <viniciusnakahara@gmail.com>
2023-12-05 23:52:58 -0300

    Alteraçõe Convertendo pdf para docx desformatado

--- src/pdf/controllers/pdfController.ts
+++ src/pdf/controllers/pdfController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 import pdf from 'pdf-parse';
 import  { PDFParse } from '../interface/pdfParse';
-import { Document, Packer, Paragraph } from 'docx';
+import * as fs from 'fs';
+import { Document, Packer, Paragraph, TextRun } from 'docx';
 
 export const convertPdfToDocx = async (req: Request, response: Response): Promise<void> => {
   try {
@@ -11,6 +12,7 @@ export const convertPdfToDocx = async (req: Request, response: Response): Promis
       return;
     }    
     pdf(pdfBuffer).then((documentPDF) => {
+      response.json(pdfBuffer.toString('base64'));
       sendPdfAsDocxResponse(documentPDF, response)
     })
   } catch (error) {
@@ -21,18 +23,13 @@ export const convertPdfToDocx = async (req: Request, response: Response): Promis
 
 export const convertBase64ToDocx = async(req: Request, response: Response): Promise<void> => {
   try {
-    console.log("Passou aqui ");
     const base64String = req.body.base64String;
     if (!base64String) {
       response.status(400).send('No Base64 string provided.');
       return;
     }
-    
-    console.log("Passou aqui 1")
     const pdfBuffer = Buffer.from(base64String, 'base64');
-    console.log("Passou aqui 2")
     const extractedText = await extractTextFromPDF(pdfBuffer);
-    console.log("Passou aqui 3")
     const docxBuffer = await createDocxFromText(extractedText);
 
     response.setHeader('Content-Disposition', 'attachment; filename=converted.docx');
@@ -54,31 +51,32 @@ const createDocxFromText = async (text: string) => {
   console.log(text);
   const doc = new Document({
     sections: [{
-      children: text.split('\n').map(line => new Paragraph(line)),
+      children: text.split('\n').map(line => new Paragraph({ children: [new TextRun(line.trim())] })),
     }],
   });
 
   return await Packer.toBuffer(doc);
 };
 
-const sendPdfAsDocxResponse : any = async (documentPDF: PDFParse, response: Response) => {
-    const docx = new Document({
+const sendPdfAsDocxResponse : any = async (documentPDF: any, response: Response) => {
+  const docxTrim = documentPDF.trim();  
+  
+  const docx = new Document({
         sections: [{
             children: documentPDF.text.split('\n').map(buildPDF),
         }],
     });
+
+    console.log(documentPDF.text);
     const docxBuffer = await Packer.toBuffer(docx);
+
+    fs.writeFileSync('D:\Conversor\src\docx.docx', docxBuffer);
     response.setHeader('Content-Disposition', 'attachment; filename=converted.docx');
     response.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
     return response.end(docxBuffer);
-  } 
+} 
 
 const buildPDF : any = (text: string) => {
-  const adjustedText =   text.replace(/([a-z])([A-Z][a-z])/g, '$1 $2') // Entre minúsculas e maiúsculas seguido por minúsculas
-  .replace(/([0-9])([a-zA-Z])/g, '$1 $2')  // Entre números e letras
-  .replace(/([a-zA-Z])([0-9])/g, '$1 $2'
-  ); // Entre letras e números
-
-  console.log(adjustedText);
-  return new Paragraph(adjustedText);
+  console.log(text.trim());
+  return new Paragraph(text);
 }
--- src/pdf/routers/pdfRoutes.ts
+++ src/pdf/routers/pdfRoutes.ts
@@ -7,6 +7,6 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 pdfRouter.post('/convert-pdf-docx',upload.single('file') ,convertPdfToDocx);
-pdfRouter.post('convert-base64-docx',convertBase64ToDocx);
+pdfRouter.post('/convert-base64-docx',convertBase64ToDocx);
 
 export default pdfRouter;
